2ab6b00e55651d6a1a07ac773322fa3a70d76d25
nikolas-poberezhniy <nikolas.poberezhniy@gmail.com>
2022-08-14 13:38:30 +0300

    rewrite 3 task with creating unique promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,46 +8,33 @@ form.addEventListener('submit', e => {
   const amount = parseInt(e.currentTarget.elements['amount'].value);
 
   startGeneratePromise(delay, step, amount);
+  e.currentTarget.reset;
 });
-
-function startGeneratePromise(delay, step, amount) {
-  setTimeout(() => {
-    promiseRepeating(step, amount, delay);
-  }, delay);
-}
-
-function promiseRepeating(step, amount, delay) {
-  let currentStep = 1;
-  let sumOfDelay = delay;
-
-  const timerID = setInterval(() => {
-    if (currentStep === amount) {
-      clearInterval(timerID);
-    }
-
-    outputPromiseResult(currentStep, sumOfDelay);
-    currentStep += 1;
-    sumOfDelay += step;
-  }, step);
-}
-
-function outputPromiseResult(position, daley) {
-  createPromise(position, daley)
-    .then(({ position, delay }) => {
-      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
-}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+  console.log(shouldResolve);
   return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
+}
+
+function startGeneratePromise(delay, step, amount) {
+  for (let i = 1; i <= amount; i++) {
+    const promise = createPromise(i, delay);
+    promise
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
 }
